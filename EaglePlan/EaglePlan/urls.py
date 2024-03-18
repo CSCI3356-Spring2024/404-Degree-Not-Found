c9a779a5f47f04f7c824c8a81e11fe0e409d720e
Matt Murno <mattmurno@gmail.com>
2024-03-18 19:08:12 -0400

    views changing and adding landing page

--- EaglePlan/EaglePlan/urls.py
+++ EaglePlan/EaglePlan/urls.py
@@ -1,12 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.contrib.auth.views import LoginView, LogoutView
-from Plan.views import SignUpView
-from django.contrib.auth import views as auth_views
-from Plan import views
+from django.urls import include, path
 
+#include() chops off whatever part of the URL matched up to that point and sends the remaining string to the included URLconf for further processing.
 urlpatterns = [
+    path("Plan/", include("Plan.urls")),
     path('admin/', admin.site.urls),
-    path('accounts/returning/', auth_views.LoginView.as_view(), name='returning'),
-    path('signup/', views.SignUpView.as_view(), name='signup'),
 ]
--- EaglePlan/Plan/views.py
+++ EaglePlan/Plan/views.py
@@ -4,14 +4,18 @@ from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 
+# class SignUpView(generic.CreateView):
+#     form_class = UserCreationForm
+#     success_url = reverse_lazy('login')
+#     template_name = 'signup.html'
 
-class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'signup.html'
+# def home_view(request, *args, **kwargs):
+# 	print(args, kwargs)
+# 	print(request.user)
+# 	#return HttpResponse('<h1>Hello World</h1>') #string of HTML code
+# 	return render(request, 'home.html', {}) 
 
-def home_view(request, *args, **kwargs):
-	print(args, kwargs)
-	print(request.user)
-	#return HttpResponse('<h1>Hello World</h1>') #string of HTML code
-	return render(request, 'home.html', {}) 
+class LandingView(generic.ListView):
+    #links to the html code in "polls/index.html"
+    template_name = "plan/Landing.html"
+		
