a108d3e45cd398440448f6e6fb63275480508175
Matt Murno <mattmurno@gmail.com>
2024-03-18 19:58:46 -0400

    trying to fix signup

--- EaglePlan/EaglePlan/settings.py
+++ EaglePlan/EaglePlan/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "Plan.apps.PlanConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- EaglePlan/Plan/views.py
+++ EaglePlan/Plan/views.py
@@ -4,18 +4,21 @@ from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 
-# class SignUpView(generic.CreateView):
-#     form_class = UserCreationForm
-#     success_url = reverse_lazy('login')
-#     template_name = 'signup.html'
+from .models import Name
+class SignUpView(generic.CreateView):
+	template_name = '/Registration/signup.html'
 
-# def home_view(request, *args, **kwargs):
+	form_class = UserCreationForm
+	success_url = reverse_lazy('login')
+
+#def home_view(request, *args, **kwargs):
 # 	print(args, kwargs)
 # 	print(request.user)
 # 	#return HttpResponse('<h1>Hello World</h1>') #string of HTML code
 # 	return render(request, 'home.html', {}) 
 
-class LandingView(generic.ListView):
-    #links to the html code in "polls/index.html"
-    template_name = "plan/Landing.html"
-		
+class LandingView(generic.CreateView):
+     #links to the html code in "plan/Landing.html"
+ 	template_name = "plan/Landing.html"
+# 	model = Name
+
