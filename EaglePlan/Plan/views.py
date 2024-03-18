19d3a301784e3b5ec2b07d7616b88e00d40983d6
board77 <59212592+board77@users.noreply.github.com>
2024-03-18 18:14:42 -0400

    views mod

--- EaglePlan/Plan/views.py
+++ EaglePlan/Plan/views.py
@@ -1,14 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 
-@login_required
-def product_delete(request, id):
-    product = get_object_or_404(Product, id=id)
-    if request.method == "POST":
-        product.delete()
-        return redirect('product_list')
-    return render(request, "product_confirm_delete.html", {'product': product})
-
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
