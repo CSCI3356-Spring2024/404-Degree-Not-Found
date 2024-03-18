fa6a68cc673aa6e6c039926625ad32991afd46c6
board77 <59212592+board77@users.noreply.github.com>
2024-03-18 18:12:54 -0400

    views addition

--- EaglePlan/Plan/views.py
+++ EaglePlan/Plan/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
 
-# Create your views here.
+@login_required
+def product_delete(request, id):
+    product = get_object_or_404(Product, id=id)
+    if request.method == "POST":
+        product.delete()
+        return redirect('product_list')
+    return render(request, "product_confirm_delete.html", {'product': product})
+
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
+
+def home_view(request, *args, **kwargs):
+	print(args, kwargs)
+	print(request.user)
+	#return HttpResponse('<h1>Hello World</h1>') #string of HTML code
+	return render(request, 'home.html', {})
