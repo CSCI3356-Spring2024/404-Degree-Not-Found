12cf2523aea34fc6f66e1b329bd98b806e684545
board77 <59212592+board77@users.noreply.github.com>
2024-03-18 18:13:50 -0400

    views addition again

--- EaglePlan/Plan/views.py
+++ EaglePlan/Plan/views.py
@@ -18,4 +18,4 @@ def home_view(request, *args, **kwargs):
 	print(args, kwargs)
 	print(request.user)
 	#return HttpResponse('<h1>Hello World</h1>') #string of HTML code
-	return render(request, 'home.html', {})
+	return render(request, 'home.html', {}) 
