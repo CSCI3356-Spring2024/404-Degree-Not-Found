6083167a160df3564c6b1af9ca165cd3b6492f5b
Andy Jung <jungkj@bc.edu>
2024-03-18 18:32:51 -0400

    updated views

--- EaglePlan/Plan/views.py
+++ EaglePlan/Plan/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views import generic 
+
+
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
