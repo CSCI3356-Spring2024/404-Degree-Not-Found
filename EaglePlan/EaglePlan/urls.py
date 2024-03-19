e08bb272c5793f720c92a10318ebc729239f8237
Andy Jung <jungkj@bc.edu>
2024-03-18 20:12:22 -0400

    Update Signup

--- EaglePlan/EaglePlan/urls.py
+++ EaglePlan/EaglePlan/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
-
+from Plan.views import SignUpView
+from Plan import views
 #include() chops off whatever part of the URL matched up to that point and sends the remaining string to the included URLconf for further processing.
 urlpatterns = [
     path("Plan/", include("Plan.urls")),
     path('admin/', admin.site.urls),
+    path('signup/', views.SignUpView.as_view(), name='signup'),
 ]
--- EaglePlan/Plan/views.py
+++ EaglePlan/Plan/views.py
@@ -4,9 +4,13 @@ from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 
+<<<<<<< HEAD
 from .models import Name
 class SignUpView(generic.CreateView):
 	template_name = '/Registration/signup.html'
+=======
+
+>>>>>>> a06ce33 (Signup Page)
 
 	form_class = UserCreationForm
 	success_url = reverse_lazy('login')
@@ -17,8 +21,19 @@ class SignUpView(generic.CreateView):
 # 	#return HttpResponse('<h1>Hello World</h1>') #string of HTML code
 # 	return render(request, 'home.html', {}) 
 
+<<<<<<< HEAD
 class LandingView(generic.CreateView):
      #links to the html code in "plan/Landing.html"
  	template_name = "plan/Landing.html"
 # 	model = Name
 
+=======
+class LandingView(generic.ListView):
+    #links to the html code in "polls/index.html"
+    template_name = "plan/Landing.html"
+		
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
+>>>>>>> a06ce33 (Signup Page)
