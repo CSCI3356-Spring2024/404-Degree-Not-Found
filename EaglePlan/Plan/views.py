fb2ef950e488f5c8443fbbd1aa9b9a3c70f93720
Andy Jung <jungkj@bc.edu>
2024-03-18 20:04:21 -0400

    Signup Page

--- EaglePlan/Plan/views.py
+++ EaglePlan/Plan/views.py
@@ -4,13 +4,9 @@ from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 
-<<<<<<< HEAD
 from .models import Name
 class SignUpView(generic.CreateView):
 	template_name = '/Registration/signup.html'
-=======
-
->>>>>>> a06ce33 (Signup Page)
 
 	form_class = UserCreationForm
 	success_url = reverse_lazy('login')
@@ -21,13 +17,6 @@ class SignUpView(generic.CreateView):
 # 	#return HttpResponse('<h1>Hello World</h1>') #string of HTML code
 # 	return render(request, 'home.html', {}) 
 
-<<<<<<< HEAD
-class LandingView(generic.CreateView):
-     #links to the html code in "plan/Landing.html"
- 	template_name = "plan/Landing.html"
-# 	model = Name
-
-=======
 class LandingView(generic.ListView):
     #links to the html code in "polls/index.html"
     template_name = "plan/Landing.html"
@@ -36,4 +25,3 @@ class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
->>>>>>> a06ce33 (Signup Page)
