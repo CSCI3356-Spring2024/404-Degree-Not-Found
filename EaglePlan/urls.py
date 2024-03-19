92d9877b399729e468e9f7b60b0f63d99618b5a4
shumhbc <shumh@bc.edu>
2024-03-19 10:45:34 -0400

    project url VS app url should be separate for modularity

--- EaglePlan/urls.py
+++ EaglePlan/urls.py
@@ -3,8 +3,6 @@ from django.urls import include, path
 from Plan import views
 #include() chops off whatever part of the URL matched up to that point and sends the remaining string to the included URLconf for further processing.
 urlpatterns = [
-    path("", views.landing_view, name="landing"),
-    path("Plan/", include("Plan.urls")),
     path('admin/', admin.site.urls),
-    path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('', include('Plan.urls')),
 ]
