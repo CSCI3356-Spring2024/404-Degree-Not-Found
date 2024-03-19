a539867040a87410b302400f33e81489753cd37e
Matt Murno <mattmurno@gmail.com>
2024-03-18 21:18:18 -0400

    building landing page

--- Plan/views.py
+++ Plan/views.py
@@ -16,7 +16,7 @@ def landing_view(request, *args, **kwargs):
 	print(args, kwargs)
 	print(request.user)
  	#return HttpResponse('<h1>Hello World</h1>') #string of HTML code
-	return render(request, 'plan/Landing.html', {}) 
+	return render(request, 'Landing.html', {}) 
 
 
 #class LandingView(generic.CreateView):
