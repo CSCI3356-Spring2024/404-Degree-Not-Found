ce2ff30d85f747afad059ee6c029d5c9df99c394
iwasemo <iwasemo@bc.edu>
2024-03-18 20:40:22 -0400

    add landing page to urls

--- EaglePlan/urls.py
+++ EaglePlan/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
-from Plan.views import SignUpView
 from Plan import views
 #include() chops off whatever part of the URL matched up to that point and sends the remaining string to the included URLconf for further processing.
 urlpatterns = [
+    path("", views.LandingView.as_view(), name="landing"),
     path("Plan/", include("Plan.urls")),
     path('admin/', admin.site.urls),
     path('signup/', views.SignUpView.as_view(), name='signup'),
