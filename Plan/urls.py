258bb133865a3a4ad47e20e4fc41df1312cd8c92
shumhbc <shumh@bc.edu>
2024-03-19 20:42:36 -0400

    urls

--- Plan/urls.py
+++ Plan/urls.py
@@ -13,7 +13,7 @@ app_name = "Plan"
 urlpatterns = [
     path("", views.landing_view, name="landing"),
     path('admin/', admin.site.urls),
-    path('login/', views.login_view, name='login'),
+    #path('login/', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
     path('landing/', views.landing_view, name='landing'),
     path('profile/', views.profile_view, name='profile'),
