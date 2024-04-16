6f59acfc4031859430b69d1994f68697f178f2c1
iwasemo <iwasemo@bc.edu>
2024-04-15 22:49:42 -0400

    course, semester, plan visible for admin

--- Plan/admin.py
+++ Plan/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Student, Advisor
+from .models import Student, Advisor, Course, Semester, Plan
 
 admin.site.register(Student)
 admin.site.register(Advisor)
+admin.site.register(Course)
+admin.site.register(Semester)
+admin.site.register(Plan)
